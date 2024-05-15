1c80e965ac4f60013cffcb2606ba403035f6d3f5
Aleksandr Ben <a.ben@sdventures.com>
2024-05-15 21:16:53 +0200

    hw06

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,7 +18,8 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
+        let state:string = restoreState<string>('hw6-editable-span-value', value)
+        setValue(state)
 
     }
 
